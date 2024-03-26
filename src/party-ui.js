6624a91fb6322acb59e29637c7880b0ea54f0f4e
JacksonZuramski <jacksonzuramski@gmail.com>
2024-03-25 21:09:19 -0400

    simple project 1

---
+++ src/party-ui.js
@@ -0,0 +1,171 @@
+import { html, css } from "lit";
+import { DDD } from "@lrnwebcomponents/d-d-d/d-d-d.js";
+import "@lrnwebcomponents/rpg-character/rpg-character.js";
+ 
+export class partyUI extends DDD {
+
+    static get tag() {
+        return 'party-ui';
+      }
+
+    constructor(){
+        super();
+        this.userArray = [];
+        this.maxUsers = 5;
+        this.tempUser = "";
+    }
+
+  static get styles() {
+    return [
+      super.styles,
+      css`
+      :host {
+        display: block;
+      }
+      .my-div {
+        padding: var(--ddd-spacing-5);
+        margin: var(--ddd-spacing-2) var(--ddd-spacing-0);
+        color: var(--ddd-theme-default-keystoneYellow);
+      }
+
+      .party-ui-wrapper {
+        text-align: center;
+        display: block;
+        background-color: var(--ddd-theme-default-beaver70);
+        padding: 16px;
+        border: 2px;
+        margin: 8px;
+        height: 300px;
+        width: 500px;
+      }
+
+      .characters-wrapper {
+        padding: 20px;
+        width: 350px;
+        display: flex;
+        margin: auto;
+        margin-bottom: 10px;
+        justify-content: center;
+        align-items: center;
+        border: 2px;
+      }
+
+      .user {
+        color: white;
+        font-size: 30px;
+        text-align: center;
+        padding: 0px;
+        width: 50px;
+      }
+
+      .add-user-button{
+        color: blue;
+        font-size: 20px;
+      }
+
+      .delete-button{
+        color: green;
+      }
+
+      .save-wrapper{
+        padding: 20px;
+        width: 250px;
+        display: flex;
+        flex-direction: column;
+        margin: auto;
+        margin-bottom: 20px;
+        justify-content: center;
+        align-items: center;
+        border: 2px;
+
+      }
+
+      .save-button{
+        color: green;
+      }
+
+      .bar{
+        height: 100px;
+        width: 4px;
+        background-color: black;
+        display: flex;
+      }
+
+    `];
+  }
+
+
+  updateName(event)
+  {
+    this.tempUser = event.target.value;
+
+  }
+
+  addUser(event){
+    if (this.userArray.length < this.maxUsers) {
+      this.userArray.push(this.tempUser);
+      this.shadowRoot.querySelector('.text-input').value = "";
+      this.requestUpdate();
+  }
+
+
+}
+
+deleteUser() {
+
+  if (this.userArray.length > 0) {
+      this.userArray.pop();
+  }
+
+  this.requestUpdate();
+}
+
+  characterView(name){
+    // console.log(name + " is running")
+
+    return html`
+        <rpg-character id="rpg" hat="random" seed= ${name} style= "height: 100px; width: 100px;"></rpg-character>
+    `;
+  }
+
+  render() {
+    
+    return html`
+    <div class="party-ui-wrapper">
+      <div class="usernames-wrapper">
+          <input class="text-input" type="text" value=${this.user} @input=${this.updateName}>
+          <button id="add-user-button" @click="${this.addUser}" ?disabled="${this.userArray.length >= this.maxUsers}">Add User</button>
+      </div>
+      <div class="characters-wrapper">
+          ${this.userArray.map(element => 
+            this.characterView(element)
+          )}
+
+        <div class="bar"></div>
+        ${this.characterView(this.tempUser)}
+
+        </div>
+
+          <div class="user"></div>
+      <div class="save-wrapper"></div>
+        <button id="save-button">Save</button>
+        <button id="delete-button" @click="${this.deleteUser}">Delete</button>
+        </div>
+      </div>
+    </div>
+    `;
+  }
+ 
+  static get properties() {
+    return {
+      ...super.properties,
+      title: { type: String },
+      user: { type: String, reflect: true},
+      userArray: { type: Array, reflect: true},
+      maxUsers: { type: Number},
+      tempUser: { type: String, reflect: true},
+    }
+  }
+}
+
+globalThis.customElements.define(partyUI.tag, partyUI);
--- src/xalert.js
+++ src/xalert.js
@@ -14,4 +14,86 @@ export class Alert extends LitElement {
     this.opened = true;
   }
 
-  
+  static get styles() {
+    return css`
+      :host {
+        font-size: 16px; 
+      }
+      :host([number="25"]){
+        color: orange;
+      }
+
+      .sticky {
+        position: sticky;
+        top: 0;
+        z-index: 100;
+        opacity: 1.0;
+      }
+
+      .campus-alert {
+        font-size: 1em;
+        display: block;
+        padding: 16px;
+        margin: 16px;
+      }
+
+      .message {
+        font-size: 20px;
+        color: white;
+      }
+    `;
+  }
+
+  openedView(color) {
+    return html `
+      <div class="campus-alert ${(this.sticky) ? "sticky" : ""}" style="background-color:${color}">
+        <p class="message">${this.message}</p>
+      </div>
+    `;
+  }
+
+  closedView() {
+    return html `
+      <div>This is closed</div>
+    `;
+  }
+
+  render() {
+    let color = "white";
+    if(this.issueLevel === "notice") color = "green";
+    if(this.issueLevel === "warning") color = "orange";
+    if(this.issueLevel === "alert") color = "red";
+    if(this.issueLevel === "welcome") color = "grey";
+
+    if(this.issueLevel === "welcome") (this.message) = "Welcome!";
+    if(this.issueLevel === "notice") (this.message) = "Small notice: watch out for this today.";
+    if(this.issueLevel === "warning") (this.message) = "This is a warning. Exceed some caution."
+    if(this.issueLevel === "alert") (this.message) = "This is an alert. PROCEED WITH IMMENSE CAUTION!";
+    
+    return (this.opened) ? this.openedView(color) : this.closedView();
+  }
+
+  static get properties() {
+    return {
+      issueLevel: { type: String },
+      message: { type: String },
+      sticky: { type: Boolean },
+      opened: { type: Boolean, reflect: true },
+    };
+  }
+
+  toggle() {
+    this.opened = !this.opened;
+    if (!this.opened) {
+      localStorage.setItem('alertClosed', 'true');
+    } else {
+      localStorage.removeItem('alertClosed');
+    }
+    const oppositeButton = this.shadowRoot.querySelector('.btn');
+    if (oppositeButton) {
+      oppositeButton.focus();
+    }
+  }
+}
+
+customElements.define(Alert.tag, Alert);
